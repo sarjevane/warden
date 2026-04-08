26c5944c375018727424bff5347c46294ac009c5
tbadts <thomas.badts@pasqal.com>
2026-04-08 12:31:20 +0000

    worker.py refactoring

--- warden/scheduler/worker.py
+++ warden/scheduler/worker.py
@@ -18,11 +18,11 @@ class LocalQPUWorker:
 
     def __init__(self, conf: Config):
         self.conf_sched = conf.scheduler
-        self.client = QPUClient(qpu_conf=conf.qpu)
+        self.qpu_client = QPUClient(qpu_conf=conf.qpu)
 
     @property
     def operational_status(self):
-        return self.client.get_operational_status()
+        return self.qpu_client.get_operational_status()
 
     @property
     def is_operational(self):
@@ -42,17 +42,17 @@ class LocalQPUWorker:
     async def execute_job(
         self, queue: MemQueue, nb_run: int, sequence: str, batch_id: str | None = None
     ) -> None:
-        """Submit job to run on the QPU"""
+        """Execute job on the QPU"""
 
-        # Object to store job information to send to the db
+        # Object to store job status and information to send to the db
         qpu_job = QPUJobInfo()
 
-        qpu_job = await self._poll_qpu(queue=queue, qpu_job=qpu_job)
+        qpu_job = await self.poll_qpu(queue=queue, qpu_job=qpu_job)
         if qpu_job.status == "ERROR":
             return
         logger.info("QPU is operational")
 
-        qpu_job = await self._create_job(
+        qpu_job = await self.create_job(
             queue=queue,
             qpu_job=qpu_job,
             nb_run=nb_run,
@@ -63,12 +63,12 @@ class LocalQPUWorker:
             return
         logger.info("Job created on QPU")
 
-        qpu_job = await self._poll_job(queue, qpu_job)
+        qpu_job = await self.await_job_execution(queue, qpu_job)
         if qpu_job.status not in ("CANCELED", "ERROR"):
             logger.info("Job execution done")
-        return
 
-    async def _poll_qpu(self, queue: MemQueue, qpu_job: QPUJobInfo) -> QPUJobInfo:
+    async def poll_qpu(self, queue: MemQueue, qpu_job: QPUJobInfo) -> QPUJobInfo:
+        """Check the QPU status"""
         try:
             polling_start = datetime.now()
             while not self.is_operational:
@@ -94,7 +94,7 @@ class LocalQPUWorker:
             await queue.put(qpu_job)
         return qpu_job
 
-    async def _create_job(
+    async def create_job(
         self,
         queue: MemQueue,
         qpu_job: QPUJobInfo,
@@ -102,10 +102,9 @@ class LocalQPUWorker:
         sequence: int,
         batch_id: str,
     ) -> QPUJobInfo:
+        """Create the job on the QPU"""
         try:
-            # TODO: 400 error when QPU is not UP ?
-            # TODO: handling job already created
-            qpu_job = self.client.create_job(
+            qpu_job = self.qpu_client.create_job(
                 nb_run=nb_run,
                 abstract_sequence=sequence,
                 batch_id=batch_id,
@@ -116,7 +115,10 @@ class LocalQPUWorker:
             await queue.put(qpu_job)
         return qpu_job
 
-    async def _poll_job(self, queue: MemQueue, qpu_job: QPUJobInfo) -> QPUJobInfo:
+    async def await_job_execution(
+        self, queue: MemQueue, qpu_job: QPUJobInfo
+    ) -> QPUJobInfo:
+        """Polling the job status untill completion, error, or cancellation"""
         polling_start = datetime.now()
         qpu_job = self._get_job_poll(qpu_job)
         await queue.put(qpu_job)
@@ -128,15 +130,15 @@ class LocalQPUWorker:
                     f"{qpu_job.uid}."
                 )
                 try:
-                    qpu_job = self.client.cancel_job(qpu_job)
+                    qpu_job = self.qpu_client.cancel_job(qpu_job)
                 except QPUClientRequestError as e:
                     logger.error(f"Failed cancelling job: {e}")
                     qpu_job = self.qpu_job_to_error(qpu_job)
                     await queue.put(qpu_job)
-                    break
+                    continue
                 logger.info("Job cancellation done")
                 await queue.put(qpu_job)
-                break
+                continue
             await asyncio.sleep(self.conf_sched.job_polling_interval_s)
             qpu_job = self._get_job_poll(qpu_job)
             await queue.put(qpu_job)
@@ -147,7 +149,7 @@ class LocalQPUWorker:
             # When polling the job status, we set no_retry=True as we are
             # in the job polling loop that will handle the retry of the requests
             # until an eventual timout of the job
-            qpu_job = self.client.get_job(qpu_job, no_retry=True)
+            qpu_job = self.qpu_client.get_job(qpu_job, no_retry=True)
             logger.info(f"Job status: {qpu_job.status}")
         except QPUClientRequestError as e:
             logger.warning(
