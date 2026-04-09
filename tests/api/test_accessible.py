76f8d8ca7626f5078cae75944f0c937d2531418b
tbadts <thomas.badts@pasqal.com>
2026-04-09 08:33:18 +0000

    Remove mock munge from accessible endpoint

--- tests/api/test_accessible.py
+++ tests/api/test_accessible.py
@@ -1,5 +1,4 @@
 import pytest
-from conftest import mock_munge_auth
 from httpx import AsyncClient
 
 
@@ -7,8 +6,7 @@ from httpx import AsyncClient
 async def test_accessible_nominal(client: AsyncClient, app):
     """Test nominal behavior where warden is accessible"""
 
-    # Call to the endpoint as root user from spank plugin
-    with mock_munge_auth(app, uid=0):
-        response = await client.get("/accessible")
+    # Call to the endpoint
+    response = await client.get("/accessible")
     assert response.status_code == 200
     assert response.json()["is_accessible"]
